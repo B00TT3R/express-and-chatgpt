b3e3dba9dd71acf3ade07c69afe05240da510fb7
B00TT3R <boott3r@gmail.com>
2024-05-05 11:35:38 -0400

    websocket

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ import dotenv from 'dotenv'
 import path from 'path'
 import cors from "cors"
 import { fileURLToPath } from 'url';
+import { WebSocketServer } from 'ws';
 
 import knowledge from "./src/generators/knowledge.ts"
 import removeQuotes from "./src/utils/removeQuotes.ts"
+import isJSONValid from "./src/utils/isJSONValid.ts"
 
 dotenv.config()
 const __filename = fileURLToPath(import.meta.url);
@@ -21,10 +23,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 })
 
-app.get('/buttonClick', async (req, res) => {
-    console.log("buttonClick")
-    res.send({message:"Chamado recebido"})  
-})
+
 
 
 app.get('/knowledge', async (req, res) => {
@@ -34,6 +33,51 @@ app.get('/knowledge', async (req, res) => {
     res.send(removeQuotes(quote.choices?.[0]?.message?.content??""))
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App iniciado na porta: ${port}`)
+})
+
+const wss = new WebSocketServer({ server });
+wss.on("connection", (ws) => {
+    console.log("Nova conexão WebSocket");
+
+    
+    ws.on("message", (data) => {
+        try{
+            if (isJSONValid(data.toString())){
+                const dataValue = JSON.parse(String(data.toString()));
+                switch(dataValue.op){
+                    case "echo":
+                        wss.clients.forEach((client) => {
+                            if (client.readyState === client.OPEN) {
+                                client.send(`Mensagem ecoada: ${dataValue.message}`);
+                            }
+                        });
+                }
+            }
+            else wss.clients.forEach((client) => {
+                if (client.readyState === client.OPEN) {
+                    client.send(`Não é um json válido`);
+                }
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    });
+
+    ws.on("close", () => {
+        console.log("Conexão WebSocket fechada");
+    });
+});
+
+app.get('/buttonClick', async (req, res) => {
+    console.log("buttonClick")
+    res.send({message:"Chamado recebido"})
+    wss.clients.forEach((client) => {
+        if (client.readyState === client.OPEN) {
+            client.send(JSON.stringify({"op":"light", "message":"on"}));
+        }
+    });
+    
 })
